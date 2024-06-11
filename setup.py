74acbe4ae18986a4aebcc98a7338e3f710545c17
xyber <metythorn@gmail.com>
2024-06-11 16:01:55 +0700

    [feat]: autocorrect_gender, resize_h_128, resize_image, to_grayscale

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 
 setup(
     name="ocr-toolkits",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["tests", "tests.*"]),
     package_data={
         'ocr-tools': [
